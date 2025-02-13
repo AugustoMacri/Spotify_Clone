f85b436af18ce8096f336b7321938ead2136d883
AugustoMacri <augusto.macri@hotmail.com>
2025-02-13 20:41:43 -0300

    Backend

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,27 @@
 import React from 'react'
 import Header from './components/Header';
 import Main from './components/Main';
+import { BrowserRouter, Routes, Route} from "react-router-dom";
+import Home from './pages/Home';
+import Artist from './pages/Artist';
+import Songs from './pages/Songs';
+import Song from './pages/Song';
 
 const App = () => {
   return (
-    <>
+    <BrowserRouter>
       <Header />
-      <Main />
+
+      <Routes>
+        <Route path='/' element={<Home />}/>
+        <Route path='/artists' element={<Artist />}/>
+        <Route path='/artist/:id' element={<Artist />}/>
+        <Route path='/songs' element={<Songs />}/>
+        <Route path='/song/:id' element={<Song />}/>
+
+      </Routes>
     
-    </>
+    </BrowserRouter>
   )
 }
 
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,14 +1,18 @@
 import React from 'react'
 import logoSpotify from '../assets/logo/spotify-logo.png'
+import { Link } from 'react-router-dom'
 
 const Header = () => {
   return (
     <div className='header'>
-        <img src={logoSpotify} alt="Logo so Spotify" /> 
 
-        <a className="header__link" href="/">
-            <h1>Spotify</h1>
-        </a>
+      <Link to="/">
+        <img src={logoSpotify} alt="Logo so Spotify" />
+      </Link>
+
+      <Link to="/" className="header__link">
+        <h1>Spotify</h1>
+      </Link>
     </div>
   )
 }
